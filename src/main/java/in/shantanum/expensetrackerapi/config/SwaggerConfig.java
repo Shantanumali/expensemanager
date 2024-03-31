4766888fbab0028c60530bcc0455f4e9fb39f21b
shantanumali <shantumali95@gmail.com>
2024-03-31 18:43:12 +0530

    Added JWT authorization in swagger API

--- src/main/java/in/shantanum/expensetrackerapi/config/SwaggerConfig.java
+++ src/main/java/in/shantanum/expensetrackerapi/config/SwaggerConfig.java
@@ -1,18 +1,21 @@
 package in.shantanum.expensetrackerapi.config;
 
-import java.util.Collection;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.Documentation;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -20,12 +23,30 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+	public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    private ApiKey apiKey(){
+        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+    }
+    
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).apiInfo(getInfo()).select()
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(getInfo()).securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey())).select()
 				.apis(RequestHandlerSelectors.basePackage("in.shantanum.expensetrackerapi")).paths(PathSelectors.any())
 				.build();
 	}
+	
+    private SecurityContext securityContext(){
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth(){
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+    }
 
 	private ApiInfo getInfo() {
 		return new ApiInfo("Expense Manager Application", "This is a Expense  Manager API developed by Shantanum",
